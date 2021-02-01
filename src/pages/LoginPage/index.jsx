45f90ded76d03a6ea98b7a200b70600c19af6ba5
rebecca08370 <rebecca08370@gmail.com>
2021-02-02 00:59:17 +0800

    add login and signup

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import Styled from "styled-components";
+import { Form, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
-const StyledPage = Styled.div`
-  margin:4em;
+const StyledLogin = Styled.div`
+  padding:60px
 `;
 
-const LoginPage = () => {
-  return (
-    <StyledPage>
-      <h1>LoginPage</h1>
-    </StyledPage>
-  );
+const toPage = (history) => {
+  history.push("/");
 };
 
+const LoginPage = () => {
+  const history = useHistory();
+
+  return (
+    <StyledLogin>
+      <h1>LoginPage</h1>
+      <div>
+        <Link to={`/login`}>
+          <Button variant="info" className="mx-2">
+            會員登入
+          </Button>
+        </Link>
+        <Link to={`/signup`}>
+          <Button variant="light">加入會員</Button>
+        </Link>
+      </div>
+      <Form className="my-4">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control placeholder="使用者名稱" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="密碼" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox"></Form.Group>
+        <Button
+          variant="primary"
+          onClick={() => {
+            toPage(history);
+          }}
+        >
+          登入
+        </Button>
+      </Form>
+    </StyledLogin>
+  );
+};
 
 export default LoginPage;
--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -1,17 +1,54 @@
 import React from "react";
 import Styled from "styled-components";
+import { Form, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
-const StyledPage = Styled.div`
-  margin:4em;
+const StyledSignup = Styled.div`
+  padding:60px
 `;
 
-const SignupPage = () => {
-  return (
-    <StyledPage>
-      <h1>SignupPage</h1>
-    </StyledPage>
-  );
+const toPage = (history) => {
+  history.push("/");
 };
 
+const SignupPage = () => {
+  const history = useHistory();
+
+  return (
+    <StyledSignup>
+      <h1>SignupPage</h1>
+      <div>
+        <Link to={`/login`}>
+          <Button variant="light" className="mx-2">
+            會員登入
+          </Button>
+        </Link>
+        <Link to={`/signup`}>
+          <Button variant="info">加入會員</Button>
+        </Link>
+      </div>
+      <Form className="my-4">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="電子郵件" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control placeholder="使用者名稱" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="密碼" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox"></Form.Group>
+        <Button
+          variant="primary"
+          onClick={() => {
+            toPage(history);
+          }}
+        >
+          註冊
+        </Button>
+      </Form>
+    </StyledSignup>
+  );
+};
 
 export default SignupPage;
