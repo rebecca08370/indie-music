f2c4c1fa69226ac8267996cbb2c772ce7bf54cd3
rebecca08370 <rebecca08370@gmail.com>
2021-02-02 15:22:41 +0800

    add search bar

--- src/pages/ArtistsPage/index.jsx
+++ src/pages/ArtistsPage/index.jsx
@@ -1,30 +1,124 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Styled from "styled-components";
 import ArtistCard from "../../components/artist/ArtistCard";
 import { artistData } from "../../utils/artist";
+import "antd/dist/antd.css";
+import { Skeleton } from "antd";
 
 const StyledPage = Styled.div`
   margin:4em;
 `;
 
-const StyledArtist = Styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center
+const StyledSearch = Styled.div`
+  padding-bottom:40px;
 `;
+
+const StyledArtistCard = Styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
+
+const SearchBar = ({ input: keyword, onChange: setKeyword }) => {
+  const BarStyling = {
+    width: "20rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+  };
+  return (
+    <input
+      style={BarStyling}
+      key="random1"
+      value={keyword}
+      placeholder={"Search artist name"}
+      onChange={(e) => setKeyword(e.target.value)}
+    />
+  );
+};
+
+const ArtistList = ({ artistList = [] }) => {
+  return (
+    <>
+      {artistList.map((props) => {
+        if (props) {
+          return <ArtistCard props={props}></ArtistCard>;
+        }
+        return null;
+      })}
+    </>
+  );
+};
+
 const ArtistsPage = () => {
+  const [input, setInput] = useState("");
+  const [artistListDefault, setArtistListDefault] = useState();
+  const [artistList, setArtistList] = useState();
+
+  const updateInput = async (input) => {
+    const filtered = artistListDefault.filter((user) => {
+      return user.artist.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    setArtistList(filtered);
+  };
+
+  const [allArtistState, setAllArtistState] = useState({
+    loading: true,
+    error: null,
+    data: [],
+  });
+
+  useEffect(() => {
+    setAllArtistState({
+      error: null,
+      data: null,
+      loading: true,
+    });
+    fetch("../../utils/artist.json")
+      .then((res) => {
+        setAllArtistState({
+          error: null,
+          data: res,
+          loading: false,
+        });
+        setArtistList(artistData);
+        setArtistListDefault(artistData);
+      })
+      .catch((err) => {
+        allArtistState({
+          error: err,
+          data: null,
+          loading: false,
+        });
+        console.error(err);
+      });
+  }, []);
+
+  if (allArtistState.error) {
+    return <h1>Not found</h1>;
+  }
+  if (allArtistState.loading || !allArtistState.data) {
+    return (
+      <div>
+        <Skeleton />
+      </div>
+    );
+  }
+
   return (
     <StyledPage>
       <div>
         <h1>ArtistsPage</h1>
       </div>
-      <h2>所有演出者</h2>
-      <h3>目前共有：{artistData && artistData.length} 個演出者</h3>
-      <StyledArtist>
-        {artistData.map((props) => {
-          return <ArtistCard props={props} key={props.artists} />;
-        })}
-      </StyledArtist>
+      <h2>共有 {artistListDefault && artistListDefault.length} 位users</h2>
+      <h4>搜尋結果有 {artistList && artistList.length} 位users</h4>
+      <StyledSearch>
+        <SearchBar input={input} onChange={updateInput} />
+      </StyledSearch>
+      <StyledArtistCard>
+        <ArtistList artistList={artistList}/>
+      </StyledArtistCard>
     </StyledPage>
   );
 };
--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,30 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EventCard from "../../components/event/EventCard";
 import Styled from "styled-components";
 import { eventData } from "../../utils/event";
+import "antd/dist/antd.css";
+import { Skeleton } from "antd";
 
 const StyledPage = Styled.div`
   margin:4em;
 `;
 
-const StyledEvent = Styled.div`
-  margin:4em;
+const StyledSearch = Styled.div`
+  padding-bottom:40px;
 `;
 
+const StyledEventCard = Styled.div`
+`;
+
+const SearchBar = ({ input: keyword, onChange: setKeyword }) => {
+  const BarStyling = {
+    width: "20rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+  };
+  return (
+    <input
+      style={BarStyling}
+      key="random1"
+      value={keyword}
+      placeholder={"Search event name"}
+      onChange={(e) => setKeyword(e.target.value)}
+    />
+  );
+};
+
+const EventList = ({ eventList = [] }) => {
+  return (
+    <>
+      {eventList.map((props) => {
+        if (props) {
+          return <EventCard props={props}></EventCard>;
+        }
+        return null;
+      })}
+    </>
+  );
+};
+
 const EventsPage = () => {
+  const [input, setInput] = useState("");
+  const [eventListDefault, setEventListDefault] = useState();
+  const [eventList, setEventList] = useState();
+
+  const updateInput = async (input) => {
+    const filtered = eventListDefault.filter((user) => {
+      return user.event.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    setEventList(filtered);
+  };
+
+  const [allEventState, setAllEventState] = useState({
+    loading: true,
+    error: null,
+    data: [],
+  });
+
+  useEffect(() => {
+    setAllEventState({
+      error: null,
+      data: null,
+      loading: true,
+    });
+    fetch("../../utils/event.json")
+      .then((res) => {
+        setAllEventState({
+          error: null,
+          data: res,
+          loading: false,
+        });
+        setEventList(eventData);
+        setEventListDefault(eventData);
+      })
+      .catch((err) => {
+        setAllEventState({
+          error: err,
+          data: null,
+          loading: false,
+        });
+        console.error(err);
+      });
+  }, []);
+
+  if (allEventState.error) {
+    return <h1>Not found</h1>;
+  }
+  if (allEventState.loading || !allEventState.data) {
+    return (
+      <div>
+        <Skeleton />
+      </div>
+    );
+  }
+  console.log(eventList);
   return (
     <StyledPage>
       <div>
         <h1>EventsPage</h1>
       </div>
       <div>
-        <h2>所有活動</h2>
-        <h3>目前共有：{eventData && eventData.length} 個活動</h3>
-        <StyledEvent>
-          {eventData.map((props) => {
-            return <EventCard props={props} key={props.eventName} />;
-          })}
-        </StyledEvent>
+        <h2>共有 {eventListDefault && eventListDefault.length} 個events</h2>
+        <h4>搜尋結果有 {eventList && eventList.length} 個events</h4>
+        <StyledSearch>
+          <SearchBar input={input} onChange={updateInput} />
+        </StyledSearch>
+        <StyledEventCard>
+          <EventList eventList={eventList} />
+        </StyledEventCard>
       </div>
     </StyledPage>
   );
